c731e4e2beeab5932f550556cabbd482a438fddc
ochiers <sebastien.ochier@gmail.com>
2015-05-11 13:13:51 +0200

    Implementation of a game, a player and the gaufre

--- preProjet/src/engine/Gaufre.java
+++ preProjet/src/engine/Gaufre.java
@@ -1,5 +1,7 @@
 package engine;
 
+import java.awt.Point;
+
 public class Gaufre {
 	
 	public static final int EMPOISONNEE = 0;
@@ -9,14 +11,26 @@ public class Gaufre {
 	public final int largeur;
 	public final int[][] grille;
 	
-	public Gaufre(int tailleLargeur, int tailleHauteur){
-		
+	public Gaufre(int tailleLargeur, int tailleHauteur){	
 		this.grille = new int[tailleLargeur][tailleHauteur];
 		this.hauteur = tailleHauteur;
 		this.largeur = tailleLargeur;
-		
+		initialisation();
 	}
-
-
+	
+	public void initialisation() {
+		for(int i  = 0; i < this.largeur; i++)
+			for (int j = 0; j < this.hauteur; j++)
+				this.grille[i][j] = Gaufre.LIBRE;
+		this.grille[0][0] = Gaufre.EMPOISONNEE; // Mise en place de la case empoisonnée en position (0,0)	
+	}
+	
+	public void setCase(Point laCase, int etat)
+	{
+		if(laCase.x < 0 || laCase.x >= largeur || laCase.y < 0 || laCase.y >= hauteur || etat > 2 || etat < 0)
+			System.err.println("Case " + laCase + " invalide, ou etat " + etat + " invalide");
+		else
+			this.grille[laCase.x][laCase.y] = etat;
+	}
 
 }
--- preProjet/src/engine/Player.java
+++ preProjet/src/engine/Player.java
@@ -1,8 +1,17 @@
 package engine;
 
-public class Player {
+import java.awt.Point;
+
+public abstract class Player {
 	
 	boolean aiPlayer;
+	public String name;
 	
-	
+	public Player(boolean isAI, String name)
+	{
+		this.aiPlayer = isAI;
+		this.name = name;
+	}
+
+	public abstract Point play();
 }
