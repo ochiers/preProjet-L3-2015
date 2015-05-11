1c6c0b9087a1a1112e2a103e11a896c5c6aa8b7e
fernajer <fernajer@127.0.0.1>
2015-05-11 15:30:13 +0200

    Refactorisation de TestMediumAI

--- preProjet/src/tests/TestMediumAI.java
+++ preProjet/src/tests/TestMediumAI.java
@@ -11,22 +11,28 @@ import engine.Engine;
 import engine.Gaufre;
 
 public class TestMediumAI {
-
 	MediumAI ia;
+	Engine engine;
+	Gaufre gaufre;
+	
+	@Before
+	public void init(){
+		engine = new Engine();
+		engine.nouvellePartie(null, ia, ia, 2, 2);
+		gaufre = engine.partieCourante.map;
+		ia = new MediumAI(engine, true, "CP1");
+	}
 	
 	@Test
-	public void test() {
-		Engine engine = new Engine();
-		engine.nouvellePartie(null, ia, ia, 2, 2);
-		Gaufre gaufre = engine.partieCourante.map;
-		ia = new MediumAI(engine, true, "CP1");
+	public void test_autre_case_libre() {
 		Point res = ia.play();
-		assertTrue(res.x == 1 && res.y == 1);
-		
+		assertTrue(res.x == 1 && res.y == 1);		
+	}
+	
+	@Test
+	public void test_pas_autre_case_libre() {
 		gaufre.grille[1][1] = Gaufre.MANGEE;
-		res = ia.play();
-		assertTrue(gaufre.grille[res.x][res.y] == Gaufre.LIBRE);
-		assertTrue((res.x == 1 && res.y == 0) || (res.x == 0 && res.y == 1));
-		
+		Point res = ia.play();
+		assertTrue(((res.x == 1 && res.y == 0) || (res.x == 0 && res.y == 1)) && gaufre.grille[res.x][res.y] == Gaufre.LIBRE);
 	}
 }
