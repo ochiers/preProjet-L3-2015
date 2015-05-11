d3e51446e3c28f9bc4b06db745a9fb3b8af046b1
crouziej <crouziej@127.0.0.1>
2015-05-11 13:45:01 +0200

    boutons

--- preProjet/src/IHM/Fenetre.java
+++ preProjet/src/IHM/Fenetre.java
@@ -5,12 +5,41 @@ import javax.swing.*;
 
 	 
 public class Fenetre implements Runnable {
+	JFrame frame = new JFrame(" Gaufre Empoisonnée ");
+	
 	public void run(){             
-		JFrame frame = new JFrame(" Gaufre Empoisonnée ");
-		frame.setSize(500, 500);      
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);        
- 		frame.setContentPane(panel);               
+		frame.setSize(1000, 1000);      
+		//panel d'accueil
+		JPanel panelAccueil = new JPanel();
+		JLabel LaGE = new JLabel(" La Gaufre Empoisonnée. ");
+		JButton newGame = new JButton(" Nouvelle Partie ");
+		JButton quit = new JButton(" Quitter ");
+		ImageIcon pointInterro = new ImageIcon("http://cdns2.freepik.com/photos-libre/question-bulle_318-77793.jpg");
+		JButton help = new JButton(" Aide ", pointInterro);
+		
+		panelAccueil.add(LaGE);
+		panelAccueil.add(newGame);
+		panelAccueil.add(quit);
+		panelAccueil.add(help);
+		
+		//
+		panelAccueil.setBackground(Color.WHITE);        
+ 		frame.add(panelAccueil);               
 		frame.setVisible(true);
 	}
 }
+
+
+/*
+		//panel choix des joueurs
+		JPanel panelJoueurs = new JPanel();
+		
+		//panel choix des difficultés
+		JPanel panelNiveaux = new JPanel();
+		
+		//panel de jeu contenant la gaufre
+		JPanel panelJeu = new JPanel();
+		
+		//panel fin de partie
+		JPanel panelFin = new JPanel();
+*/
